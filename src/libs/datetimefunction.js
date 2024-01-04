0ef03eb8ea7a50dbf06c7e8fef624cc6b4f2acd7
Daddy <daddy123@gmail.com>
2024-01-04 11:24:26 +0530

    isttimefix

--- src/libs/datetimefunction.js
+++ src/libs/datetimefunction.js
@@ -1,3 +1,30 @@
+function padWithZero(value) {
+  return value < 10 ? `0${value}` : `${value}`;
+}
+
+function convertToIST(utcTimestamp) {
+  const utcDate = new Date(utcTimestamp);
+  const istOptions = { timeZone: "Asia/Kolkata", hour12: false };
+  const istDateString = utcDate.toLocaleString("en-US", istOptions);
+
+  const [datePart, timePart] = istDateString.split(", ");
+  const [month, day, year] = datePart.split("/");
+  const [time, period] = timePart.split(" ");
+
+  const [hours, minutes, seconds] = time.split(":").map(Number);
+
+  const convertedHours = period === "PM" ? hours + 12 : hours;
+  const formattedHours = padWithZero(convertedHours);
+
+  const formattedMinutes = padWithZero(minutes);
+  const formattedSeconds = padWithZero(seconds);
+
+  return `${month}/${day}/${year} ${formattedHours}:${formattedMinutes}`;
+}
+
 export function dbTime(str) {
-  return str.replace("T", " ").substring(0, 16);
+  const istTime = convertToIST(str);
+  console.log(istTime);
+
+  return istTime;
 }
